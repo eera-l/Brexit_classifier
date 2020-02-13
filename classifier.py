9bc04b354c15b05336aaa7b52397ab8b86e3065b
Federica <federica.comuni@gmail.com>
2020-02-13 14:20:33 +0100

    added length

--- classifier.py
+++ classifier.py
@@ -7,12 +7,27 @@ from sklearn import svm
 from sklearn.model_selection import cross_validate
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.externals import joblib
+from scipy import sparse
 from sklearn.svm import LinearSVC
 from gensim import corpora
 from gensim.models.ldamodel import LdaModel
 import pandas as pd
 import numpy as np
+
+
+def read_labels(y):
+    results = []
+    for row in y:
+        labels = row.split("/")
+        labels = [num for num in labels if num is not "-1"]
+        labels = list(map(int, labels))
+        labels[0] = labels[0] * 1.2 # Assign larger weight to original annotator
+        mean = np.mean(labels)
+        results.append(1 if mean >= 0.5 else 0)
+    return y.assign(NewLabel=results)
+
 
 
 def read_file():
@@ -75,29 +90,44 @@ def perform_lda(x):
     print(ldamodel.print_topics(num_topics=2, num_words=15))
 
 
+def add_length(x):
+    scaler = MinMaxScaler()
+    data = np.array([len(line) for line in x]).reshape(-1, 1)
+    data = scaler.fit_transform(data)
+    return data
+
+
+def tfidf_vectorize(x):
+    tfidf = TfidfVectorizer(analyzer='word', ngram_range=(1, 4), stop_words='english', strip_accents='unicode')
+    x_vect = tfidf.fit_transform(x)
+    return x_vect
+
+
+def combine_tfidf_and_length(x_t, x_l):
+    x_new = sparse.hstack((x_t, x_l))
+    return x_new
+
+
 def train_classifier(clf, x, y):
-    pipeline = make_pipeline(
-        TfidfVectorizer(analyzer='word', ngram_range=(1,4), stop_words='english', strip_accents='unicode'),
-        clf)
-    pipeline.fit(x, y)
-    return pipeline
+    clf.fit(x, y)
+    name = clf.__class__.__name__
+    joblib.dump(clf, name + '.pkl')
+    return clf
 
 
 def classify(classifiers, x, y):
     train_scores = []
     test_scores = []
-    x = x['Comment']
     y = y['Label']
     for clf in classifiers:
-        pipeline = train_classifier(clf, x, y)
+        clf = train_classifier(clf, x, y)
 
-        scores = cross_validate(pipeline, x, y, cv=5,
+        scores = cross_validate(clf, x, y, cv=5,
                                 scoring=('accuracy', 'f1'),
                                 return_train_score=True)
-        name = pipeline.steps[1][0]
-
-        train_scores.append((np.mean(scores['train_accuracy']), np.mean(scores['train_f1']), name))
-        test_scores.append((np.mean(scores['test_accuracy']), np.mean(scores['test_f1']), name))
+        name = clf.__class__.__name__
+        train_scores.append((name, np.mean(scores['train_accuracy']), np.mean(scores['train_f1'])))
+        test_scores.append((name, np.mean(scores['test_accuracy']), np.mean(scores['test_f1'])))
 
     return train_scores, test_scores
 
@@ -107,7 +137,7 @@ def initialize_classifiers(x, y):
     dmc = DummyClassifier(strategy='stratified')
     svc = svm.SVC(kernel='linear')
     lr = LogisticRegression(solver='newton-cg', fit_intercept=True)
-    lsvc = LinearSVC(max_iter=2000)
+    lsvc = LinearSVC()
     mnb = MultinomialNB(alpha=2.0)
 
     classifiers = [dmc, svc, lr, lsvc, mnb]
@@ -122,8 +152,11 @@ xtr = remove_punctuation(xtr, "Comment")
 xtr = lower_case(xtr, "Comment")
 xtr = remove_stopwords(xtr, "Comment")
 xtr = lemmatize(xtr, "Comment")
-#perform_lda(xtr)
 # xtr = stem(xtr, "Comment")
-initialize_classifiers(xtr, ytr)
+x_length = add_length(xtr['Comment'])
+x_tfidf = tfidf_vectorize(xtr['Comment'])
+x_new = combine_tfidf_and_length(x_tfidf, x_length)
+#perform_lda(xtr)
+initialize_classifiers(x_new, ytr)
 
 
